74da2a560e80b9bd8de5376b12c0c5ed68f3749c
jasper-springael <56021344+jasper-springael@users.noreply.github.com>
2019-11-21 17:53:10 +0100

    added results loop and  DOM making element function

--- javascript/script.js
+++ javascript/script.js
@@ -8,14 +8,16 @@
     let inputDate = document.querySelector("#inputDate").value;
     let defaultLatLondon = "51.507351";
     let defaultLngLondon = "-0.127758";
+    let city="default";
     
+    // function deciding between default or input date
     let usedDate;
     (function chooseDate() {
         let defaultDate;
         if (inputDate == null || inputDate == "") {
             (function findDefaultDate(){
                 let newDate = new Date();
-                defaultDate = `${newDate.getFullYear()}-${(newDate.getMonth())}`;
+                defaultDate = `${newDate.getFullYear()}-${(newDate.getMonth())-1}`;
                 console.log((defaultDate));
                 return defaultDate;
             })();
@@ -23,6 +25,7 @@
         } else usedDate = inputDate;
     })();
 
+    // function deciding between default or input latitude
     let usedLat;
     (function chooseLat() {
         if (inputLat == null || inputLat == "") {
@@ -31,6 +34,7 @@
         } else return usedLat = inputLat;
     })();
     
+    // function deciding between default or input longitude
     let usedLng;
     (function chooseLng() {
         if (inputLng == null || inputLng == "") {
@@ -45,7 +49,38 @@
         console.log(usedDate);
         console.log(usedLat);
         console.log(usedLng);
-    
+
+        let resultsDomContainer = document.querySelector("#crime-event-results-container")
+        // creates a "results" header in the DOM 
+        let resultsHeader = document.createElement("h2");
+        let resultsHeaderTn = document.createTextNode(`Results for:${city}`);
+        resultsHeader.appendChild(resultsHeaderTn);
+        resultsDomContainer.appendChild(resultsHeader);
+        
+        //function that gets results out of data list, 1 by 1.
+        (function showResults(){
+            for(let i=0;i<data.length;i++) {
+                let typeOfCrime = `${data[i]["category"]}`;
+                console.log(typeOfCrime);
+                let typeOfNeighbourhood = `${data[i]["location"]["street"]["name"]}`;
+                console.log(typeOfNeighbourhood);
+                let typeOfOutcome = `${data[i]["outcome_status"]["category"]}`;
+                console.log(typeOfOutcome);
+                makeElements();
+            }
+        })();
+
+        (function makeElements(){
+            let resultsDiv = document.createElement("div");
+            let typeOfNeighbourhoodTn = document.createTextNode(`${typeOfNeighbourhood}`);
+            let typeOfCrimeTn = document.createTextNode(`${typeOfCrime}`);
+            let typeOfOutcomeTn = document.createTextNode(`${typeOfOutcome}`);
+            resultsDiv.appendChild(typeOfNeighbourhoodTn);
+            resultsDiv.appendChild(typeOfCrimeTn);
+            resultsDiv.appendChild(typeOfOutcomeTn);
+            resultsDomContainer.appendChild(resultsDiv);
+
+        })();
        
     })
     .catch(err => console.error(err));
